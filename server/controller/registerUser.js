39113f79555a69586ddfcdcafce93f17283b90f5
ShubhGithub07 <shubhamyadre@gmail.com>
2024-06-07 18:21:19 +0530

    file crashed error

---
+++ server/controller/registerUser.js
@@ -0,0 +1,44 @@
+const User = require("../models/user.model");
+const bcryptjs = require("bcryptjs");
+
+async function registerUser(request, response) {
+  try {
+    const { name, email, password, profile_pic } = request.body;
+
+    const checkEmail = await User.findOne({ email }); //{ name,email}  // null
+
+    if (checkEmail) {
+      return response.status(400).json({
+        message: "Already user exits",
+        error: true,
+      });
+    }
+
+    //password into hashpassword
+    const salt = await bcryptjs.genSalt(10);
+    const hashpassword = await bcryptjs.hash(password, salt);
+
+    const payload = {
+      name,
+      email,
+      profile_pic,
+      password: hashpassword,
+    };
+
+    const user = new User(payload);
+    const userSave = await user.save();
+
+    return response.status(201).json({
+      message: "User created successfully",
+      data: userSave,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+    });
+  }
+}
+
+module.exports = registerUser;
---
+++ server/models/message.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        default: "",
+    },
+    imageUrl: {
+        type: String,
+        default: "",
+    },
+    videoUrl: {
+        type: String,
+        default: "",
+    },
+    seen: {
+        type: Boolean,
+        default: false,
+    },
+},{ timestamps: true });
+
+export const Message = mongoose.model("Message", messageSchema);
